7105888ee5a65ced18a3ad51fcad7c0e7435d7ab
philcon93 <phil@neto.com.au>
2018-03-12 10:11:17 +1000

    added draft support

--- index.js
+++ index.js
@@ -1,69 +1,12 @@
-var GitHubApi = require('github')
+var GitHubApi = require('@octokit/rest')
 
-/*
-
-# formatNotes()
-
-Pass commit data into this function to format a markdown commit note. Data structure is based on what Github's compare endpoint returns.
-
-Example data:
-
-var dataExample = [{
-	author: {
-		login: 'brandonsheppard',
-		html_url: 'https://github.com/brandonsheppard'
-	},
-	commit: {
-		message: 'Etiam porta sem malesuada magna mollis euismod.'
-	},
-	sha: '685eb53d517a335d0d7a654472c9553679161ec8',
-	html_url: 'https://github.com/my_repository/685eb53d517a335d0d7a654472c9553679161ec8'
-},{
-	author: {
-		login: 'philconnah',
-		html_url: 'https://github.com/philconnah'
-	},
-	commit: {
-		message: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
-	},
-	sha: 'e34784dcbc43873c6900803db2e29c3555830e59',
-	html_url: 'https://github.com/my_repository/e34784dcbc43873c6900803db2e29c3555830e59'
-}]
-
-Example output:
-
-- [brandonsheppard](https://github.com/brandonsheppard) Etiam porta sem malesuada magna mollis euismod. [685eb53d517a335d0d7a654472c9553679161ec8](https://github.com/my_repository/685eb53d517a335d0d7a654472c9553679161ec8)
-- [philconnah](https://github.com/philconnah) Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. [e34784dcbc43873c6900803db2e29c3555830e59](https://github.com/my_repository/e34784dcbc43873c6900803db2e29c3555830e59)
-
-*/
-
+// Pass commit data into this function to format a markdown commit note.
+// Data structure is based on what Github's compare endpoint returns.
 function formatNotes(data){
 	return data.map(commit => `- [${commit.author.login}](${commit.author.html_url}) ${commit.commit.message} [${commit.sha}](${commit.html_url})`).join('\n')
 }
 
-/*
-
-# generateVersionNumber()
-
-This function generates a date-based version number.
-
-Year + Month + Iteration
-
-So, the first release in March 2017 will be 17.3.0
-
-If multiple releases occur in a month, the final number will increase. Otherwise, an entirely new verion number will be generated.
-
-i.e.
-generateVersionNumber('17.12.10');
-
-Output:
-
-Given that the date is 2017-12-01 (day is irrelevant)
-
-17.12.11
-
-*/
-
+// This function generates a date-based version number.
 function generateVersionNumber(previousVersion){
 	let today = new Date();
 	var newMajor = `${today.getFullYear()}.${parseInt(today.getMonth()) + 1}`
@@ -79,11 +22,9 @@ function generateVersionNumber(previousVersion){
 	return newVersion;
 }
 
-
 /* --------------- */
 
 var github = new GitHubApi();
-
 var releaseData = {
 	username: process.argv[2],
 	password: process.argv[3],
@@ -92,6 +33,7 @@ var releaseData = {
 		owner: process.argv[4],
 		name: process.argv[5]
 	},
+	'draft': process.argv[6] == 'draft' ? true : false,
 	name: ''
 }
 
@@ -121,6 +63,7 @@ github.repos.getLatestRelease({
 			repo: releaseData.repository.name,
 			tag_name: generateVersionNumber(releaseData.base),
 			name: generateVersionNumber(releaseData.base) + ' ' + releaseData.name,
+			draft: releaseData.draft,
 			body: `# All Changes \n${formatNotes(res.data.commits)}`
 		}).then(res =>
 			console.log(res)
