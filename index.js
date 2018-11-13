fd89814dda2e8e3973705cd76da0ff5aa312e936
philcon93 <phil@neto.com.au>
2018-11-13 16:11:26 +1000

    removed uneccessary code

--- index.js
+++ index.js
@@ -1,70 +1,31 @@
-var GitHubApi = require('@octokit/rest')
 
-// Pass commit data into this function to format a markdown commit note.
-// Data structure is based on what Github's compare endpoint returns.
-function formatNotes(data){
-	return data.map(commit => `- [:information_source:](${commit.html_url}) - ${commit.commit.message} - [${commit.author.login}](${commit.author.html_url})`).join('\n')
-}
 
-// This function generates a date-based version number.
+// Generates a date-based tag number
 function generateVersionNumber(previousVersion){
 	let today = new Date();
-	var newMajor = `${today.getFullYear()}.${parseInt(today.getMonth()) + 1}`
+	var newMajor = `${today.getFullYear()}.${parseInt(today.getMonth()) + 1}`;
 	newMajor = newMajor.substr(2);
-	oldMajorArr = previousVersion.split('.')
-	oldMajor = `${oldMajorArr[0]}.${oldMajorArr[1]}`
+	oldMajorArr = previousVersion.split('.');
+	oldMajor = `${oldMajorArr[0]}.${oldMajorArr[1]}`;
 
 	if(oldMajor == newMajor){
-		newVersion = `${newMajor}.${parseInt(oldMajorArr[2]) + 1}`
+		newVersion = `${newMajor}.${parseInt(oldMajorArr[2]) + 1}`;
 	} else {
-		newVersion = newMajor + '.0'
+		newVersion = newMajor + '.0';
 	}
 	return newVersion;
 }
 
-/* --------------- */
-
-var github = new GitHubApi();
-var releaseData = {
-	token: process.argv[2],
-	base: '',
-	repository: {
-		owner: process.argv[3],
-		name: process.argv[4]
-	},
-	'draft': process.argv[5] == 'draft' ? true : false,
-	name: ''
-}
-
-github.authenticate({
-	type: 'token',
-	token: releaseData.token
-})
-
-function setBase(n){
-	releaseData.base = n
+// Returns existing tag
+function setBase(tag){
+	releaseData.base = tag;
 	return n
 }
 
-github.repos.getLatestRelease({
-	owner: releaseData.repository.owner,
-	repo: releaseData.repository.name
-}).then(res =>
-	github.repos.compareCommits({
-		owner: releaseData.repository.owner,
-		repo: releaseData.repository.name,
-		head: 'master',
-		base: setBase(res.data.tag_name)
-	}).then(res =>
-		github.repos.createRelease({
-			owner: releaseData.repository.owner,
-			repo: releaseData.repository.name,
-			tag_name: generateVersionNumber(releaseData.base),
-			name: generateVersionNumber(releaseData.base) + ' ' + releaseData.name,
-			draft: releaseData.draft,
-			body: `# All Changes \n${formatNotes(res.data.commits)}`
-		}).then(res =>
-			console.log(res)
-		)
-	)
-);
+// Get latest version
+// Get current git tag
+// Get new git tag
+// Update package.json
+// Update git tag
+// Ask if want to update remote
+// git push
--- readme.md
+++ readme.md
@@ -1,59 +1,17 @@
-# Simple Github Release
+`npm i --save-dev chrono-release`
 
-This script will create a release for a specified Github repository. It will generate a version number (based on [chronological versioning](https://gist.github.com/brandonsheppard/d242ba4ba99923d332f1afdcfa4fbf86)) and release notes ([based on commit history](https://gist.github.com/brandonsheppard/548a4cff7bc1a63e69397cb82269405b)).
+# Chrono Release
+
+This script will create a release for the repository. It will generate a version number (based on [chronological versioning](https://gist.github.com/brandonsheppard/d242ba4ba99923d332f1afdcfa4fbf86)).
 
 ## Usage
 
 ```
-node index <TOKEN> <REPO_OWNER_USERNAME> <REPO_NAME> [draft]
+chrono trigger
+chrono version
 ```
-
-## Params
-
-- TOKEN - Github Personal access tokens - https://github.com/settings/tokens **required**
-- REPO_OWNER_USERNAME - Owner of the Github repo username **required**
-- REPO_NAME - Github repo name **required**
-- draft - Make a draft release instead of a public release, not including this will make the release public **optional**
 
 ## Functions
-
-### formatNotes()
-
-Pass commit data into this function to format a markdown commit note. Data structure is based on what Github's compare endpoint returns.
-
-Example data:
-
-```
-var dataExample = [{
-	author: {
-		login: 'brandonsheppard',
-		html_url: 'https://github.com/brandonsheppard'
-	},
-	commit: {
-		message: 'Etiam porta sem malesuada magna mollis euismod.'
-	},
-	sha: '685eb53d517a335d0d7a654472c9553679161ec8',
-	html_url: 'https://github.com/my_repository/685eb53d517a335d0d7a654472c9553679161ec8'
-},{
-	author: {
-		login: 'philconnah',
-		html_url: 'https://github.com/philconnah'
-	},
-	commit: {
-		message: 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.'
-	},
-	sha: 'e34784dcbc43873c6900803db2e29c3555830e59',
-	html_url: 'https://github.com/my_repository/e34784dcbc43873c6900803db2e29c3555830e59'
-}]
-```
-
-Example output:
-
-```
-[brandonsheppard](https://github.com/brandonsheppard) Etiam porta sem malesuada magna mollis euismod. [685eb53d517a335d0d7a654472c9553679161ec8](https://github.com/my_repository/685eb53d517a335d0d7a654472c9553679161ec8)
-
-[philconnah](https://github.com/philconnah) Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. [e34784dcbc43873c6900803db2e29c3555830e59](https://github.com/my_repository/e34784dcbc43873c6900803db2e29c3555830e59)
-```
 
 ### generateVersionNumber()
 
